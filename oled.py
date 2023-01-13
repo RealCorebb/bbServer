b193c44a53e794658ae25416a822b721b6226ff9
RealCorebb <coreoobb@qq.com>
2023-01-13 14:33:26 +0800

    Improve font

--- oled.py
+++ oled.py
@@ -2,9 +2,9 @@ from luma.emulator.device import pygame
 from luma.core.render import canvas
 from PIL import ImageFont, Image , ImageDraw
 import time
-font = ImageFont.truetype('Jorolks.ttf',24)
-smallJorolks = ImageFont.truetype('Jorolks.ttf',8)
-
+font = ImageFont.truetype('fonts/Jorolks.ttf',24)
+smallJorolks = ImageFont.truetype('fonts/Jorolks.ttf',10)
+seledom = ImageFont.truetype('fonts/Seledom.otf',14)
 device = pygame()
 
 def bootLogo():
@@ -14,7 +14,7 @@ def bootLogo():
         
 
 tps = 20
-cpu = 0
+cpu = 50
 playerCount = 0
 
 def mcStatus():
@@ -22,14 +22,21 @@ def mcStatus():
     with canvas(device) as draw:
         player = Image.open('icons/steve24.png')
         performance = Image.open('icons/performance.png')
-        draw.bitmap((60,5),player,fill="white")
+        draw.bitmap((69,2),player,fill="white")
         #draw.bitmap((5,5),performance,fill="white")
-        draw.text((5,5), "CPU", fill="white", font = smallJorolks)
-        draw.text((5,15), "TPS", fill="white", font = smallJorolks)
-        draw.text((90,5), str(playerCount), fill="white",font= font)
+        draw.rounded_rectangle((2,2,25,11), radius=3, fill="white")
+        draw.text((5,3), "CPU", fill="black", font = smallJorolks)
+        draw.text((28,0), str(cpu)+"%", fill="white",font = seledom)
+        draw.rounded_rectangle((2,16,25,25), radius=3, fill="white")
+        draw.text((5,17), "TPS", fill="black", font = smallJorolks)
+        draw.text((28,14), str(tps), fill="white",font = seledom)
+        draw.text((98,5), str(playerCount), fill="white",font= font)
+        #draw rounded rectangle
+        
+
 
 bootLogo()
-time.sleep(3)
+#time.sleep(3)
 mcStatus()
 while True:
     if(playerCount < 99):
