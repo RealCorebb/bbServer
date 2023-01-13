b04926d548343864df4dbec7d543954b9cfab238
RealCorebb <33654314+RealCorebb@users.noreply.github.com>
2023-01-13 22:43:47 +0800

    fix sleep

--- oled.py
+++ oled.py
@@ -18,6 +18,7 @@ def bootLogo():
     #draw image
     with canvas(device) as draw:
         draw.text((5, 20), "bbServer", fill="white", font = font)
+    time.sleep(3)
         
 
 tps = 20
@@ -26,26 +27,25 @@ playerCount = 0
 
 def mcStatus():
     global playerCount
-    
     player = Image.open('icons/steve24.png')
     performance = Image.open('icons/performance.png')
-    draw.bitmap((69,2),player,fill="white")
-    #draw.bitmap((5,5),performance,fill="white")
-    draw.rounded_rectangle((2,2,25,11), radius=3, fill="white")
-    draw.text((5,3), "CPU", fill="black", font = smallJorolks)
-    draw.text((28,0), str(cpu)+"%", fill="white",font = seledom)
-    draw.rounded_rectangle((2,16,25,25), radius=3, fill="white")
-    draw.text((5,17), "TPS", fill="black", font = smallJorolks)
-    draw.text((28,14), str(tps), fill="white",font = seledom)
-    draw.text((98,5), str(playerCount), fill="white",font= font)
-    draw.text((20,50), "Corebb killed Foambb 哈哈", fill="white",font= pixelCorebb)
+    with canvas(device) as draw:
+        
+        draw.bitmap((69,2),player,fill="white")
+        #draw.bitmap((5,5),performance,fill="white")
+        draw.rounded_rectangle((2,2,25,11), radius=3, fill="white")
+        draw.text((5,3), "CPU", fill="black", font = smallJorolks)
+        draw.text((28,0), str(cpu)+"%", fill="white",font = seledom)
+        draw.rounded_rectangle((2,16,25,25), radius=3, fill="white")
+        draw.text((5,17), "TPS", fill="black", font = smallJorolks)
+        draw.text((28,14), str(tps), fill="white",font = seledom)
+        draw.text((98,5), str(playerCount), fill="white",font= font)
+        draw.text((20,50), "Corebb killed Foambb 哈哈", fill="white",font= pixelCorebb)
+    time.sleep(10)
     #draw rounded rectangle
         
 bootLogo()
-time.sleep(3)
-bootLogo()
-time.sleep(3)
-#mcStatus()
+mcStatus()
 '''
     #threading.Timer(3.0, mcStatus(draw))
     #mcStatus(draw)
